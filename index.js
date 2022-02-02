bdf6373752b7b92a20aa708fefd89098fd5dc93b
Caleb Hamernick <caleb.hamernick@gmail.com>
2022-02-01 22:59:58 -0500

    commit tree 6915daa5c03b7ef2c218ba99f507e6d81e6b6b9a

---
+++ index.js
@@ -0,0 +1,43 @@
+function shout(string) {
+    return string.toUpperCase()
+}
+
+function whisper(string) {
+    return string.toLowerCase()
+}
+function logShout(string) {
+    console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase())
+}
+
+function sayHiToHeadphonedRoommate() {
+   
+    const answerOne = "hello";
+    const answerTwo = "HELLO";
+    const answerThree = "Let's have dinner together!";
+
+    answerOne === answerOne.toLowerCase();
+    answerTwo === answerTwo.toUpperCase();
+    
+    if (console.log(answerOne.toLowerCase()))
+    return "I can't hear you!";
+
+    else if (console.log(answerTwo.toUpperCase()))
+    return "YES INDEED!";
+
+    else if (answerThree = true)
+    return "I would love to!"; 
+
+}
+
+
+
+
+
+
+
+
+
+
